062f0021c3d197cd233d01a21ff3ead6c76c4143
Iana <malovayai@gmail.com>
2023-05-03 13:29:37 +0300

    Реалзовано добавление новой новости

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3  g-4 mx-3 mb-5 justify-content-center">
-        <div class="col mb-4" v-for="item in news" :key="item">
+        <div class="col mb-4" v-for="item in news" :key="item.id">
             <div class="card h-100 w-75">
                 <img class="card-img-top" src="https://via.placeholder.com/600x400" alt="Card image cap">
                 <div class="card-body">
@@ -12,11 +12,16 @@
             </div>
         </div>
     </div>
+    <NewsForm v-if="showForms" @add_News="add_News"></NewsForm>
 
 </template>
 
 <script>
+    import NewsForm from "@/components/NewsForm";
     export default {
+        components: {
+          NewsForm
+        },
         data() {
             return {
                 news: [
@@ -26,7 +31,13 @@
                     {id: 4, title: 'Новость 4', description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', date: '01.05.2023'},
                     {id: 5, title: 'Новость 5', description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.', date: '01.05.2023'},
                     {id: 6, title: 'Новость 6', description: 'Some quick прпарапрexмисмисмисмample text to build on the card title and make up the bulk of the card\'s content.', date: '01.05.2023'},
-                ]
+                ],
+                showForms: true
+            }
+        },
+        methods : {
+            add_News(news) {
+                this.news.push(news);
             }
         }
     }
---
+++ src/components/NewsForm.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="card mt-5">
+        <div class="card-header">
+            Добавить новость
+        </div>
+        <div class="card-body">
+            <form @submit.prevent="addNews">
+                <div class="form-group">
+                    <label for="title">Заголовок</label>
+                    <input type="text" class="form-control" id="title" v-model="title">
+                </div>
+                <div class="form-group">
+                    <label for="description">Описание</label>
+                    <textarea class="form-control" id="description" rows="3" v-model="description"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="date">Дата</label>
+                    <input type="date" class="form-control" id="date" v-model="date">
+                </div>
+                <button type="submit" class="btn btn-primary">Добавить новость</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: '',
+                description: '',
+                date: ''
+            }
+        },
+        methods: {
+            addNews() {
+                const newNews = {
+                    id: Math.random(),
+                    title: this.title,
+                    description: this.description,
+                    date: this.date
+                }
+                this.$emit('add_News', newNews)
+                this.title = ''
+                this.description = ''
+                this.date = ''
+            }
+        }
+    }
+</script>
