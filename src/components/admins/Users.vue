f63bfdfb246d0700393ddfd37d2cabe44079804f
Iana <malovayai@gmail.com>
2023-05-09 01:49:10 +0300

    Добавлена фильтрация по роли

--- src/components/admins/Users.vue
+++ src/components/admins/Users.vue
@@ -4,15 +4,29 @@
         <div class="input-group-prepend">
             <span class="input-group-text"><img src="icons/search_FILL0_wght400_GRAD0_opsz24.png" alt=""></span>
         </div>
-        <input type="text" class="form-control" placeholder="Search..." v-model="searchQuery">
-
+        <input type="text" class="form-control w-auto" placeholder="Search..." v-model="searchQuery">
+        <select class="form-select w-auto" v-model="filteredType">
+            <option value="">Все пользователи</option>
+            <option value="Пользователь">Пользователь</option>
+            <option value="Суперюзер">Суперюзер</option>
+            <option value="Администратор">Администратор</option>
+        </select>
     </div>
-    <div>
+<!--    <div class="col-sm-4">-->
+<!--        <select class="form-select" v-model="filteredType">-->
+<!--            <option value="">Все пользователи</option>-->
+<!--            <option value="Пользователь">Пользователь</option>-->
+<!--            <option value="Суперюзер">Суперюзер</option>-->
+<!--            <option value="Администратор">Администратор</option>-->
+<!--        </select>-->
+<!--    </div>-->
+    <div v-if="filteredUsers.length > 0">
         <table class="table table-bordered border-primary" style="max-width: 60%; margin-left: 30px">
             <thead>
                 <tr>
                     <th>ID</th>
                     <th>ФИО</th>
+                    <th>Роль</th>
                     <th>Логин</th>
                     <th>Пароль</th>
                     <th>Удалить</th>
@@ -23,6 +37,7 @@
                 <tr v-for="(user, index) in filteredUsers" :key="user.id">
                     <td>{{user.id}}</td>
                     <td>{{user.name}}</td>
+                    <td>{{user.role}}</td>
                     <td v-if="editIndex === index && editNow === true"><input class="form-control" type="text" v-model="user.login"></td>
                     <td v-else>{{user.login}}</td>
                     <td v-if="editIndex === index && editNow === true"><input class="form-control" type="text" v-model="user.password"></td>
@@ -34,6 +49,7 @@
             </tbody>
         </table>
     </div>
+    <div v-else><h3>Пользователи не найдены</h3></div>
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -59,7 +75,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel2">Modal title</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel2">Добавить пользователя</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
@@ -86,14 +102,15 @@
         data() {
             return {
                 listOfUsers: [
-                    { id: '1', name: 'Чеканин В.А.', login: 'ChekaninVA', password: 'qfsghy12HHG' },
-                    { id: '2', name: 'Ибатулин М.Ю.', login: 'IbatulinMU', password: 'gfgf76OP' },
-                    { id: '3', name: 'Бычкова Н.А.', login: 'BichkovaNA', password: 'fgfhgfrvdf56hgf' },
-                    { id: '4', name: 'Елисеева Н.В.', login: 'EliseevaNV', password: 'gbmkmjm454HG' },
-                    { id: '5', name: 'Колясинская Е.З', login: 'KolyasinskayaEZ', password: 'ofofgjkl66PV' },
-                    { id: '6', name: 'Мешков В.Г.', login: 'meshkovVG', password: 'fgfgf5thd34NG' },
-                    { id: '7', name: 'Петров В.Е.', login: 'PetrovVE', password: 'lpmkvk65PZX' },
-                    { id: '8', name: 'Овчинников А.И', login: 'OvchinnikovAI', password: 'jbicjbgf44GF' },
+                    { id: '1', name: 'Чеканин В.А.', role: 'Суперюзер', position:'Зав. кафедрой', login: 'ChekaninVA', password: 'qfsghy12HHG' },
+                    { id: '2', name: 'Ибатулин М.Ю.', role: 'Пользователь', position:'Зав. кафедрой', login: 'IbatulinMU', password: 'gfgf76OP' },
+                    { id: '3', name: 'Бычкова Н.А.', role: 'Пользователь', position:'Зав. кафедрой', login: 'BichkovaNA', password: 'fgfhgfrvdf56hgf' },
+                    { id: '4', name: 'Елисеева Н.В.', role: 'Пользователь', position:'Зав. кафедрой', login: 'EliseevaNV', password: 'gbmkmjm454HG' },
+                    { id: '5', name: 'Колясинская Е.З', role: 'Пользователь', position:'Зав. кафедрой', login: 'KolyasinskayaEZ', password: 'ofofgjkl66PV' },
+                    { id: '6', name: 'Мешков В.Г.', role: 'Пользователь', position:'Зав. кафедрой', login: 'meshkovVG', password: 'fgfgf5thd34NG' },
+                    { id: '7', name: 'Петров В.Е.', role: 'Пользователь', position:'Зав. кафедрой', login: 'PetrovVE', password: 'lpmkvk65PZX' },
+                    { id: '8', name: 'Овчинников А.И', role: 'Пользователь', position:'Зав. кафедрой', login: 'OvchinnikovAI', password: 'jbicjbgf44GF' },
+                    { id: '9', name: 'admin', role: 'Администратор', position:'Зав. кафедрой', login: 'admin', password: 'admin'}
 
 
                 ],
@@ -104,6 +121,7 @@
                 currentIndex: '',
                 newUser: {},
                 searchQuery: '',
+                filteredType: ''
             }
         },
         methods: {
@@ -133,7 +151,12 @@
         },
         computed: {
             filteredUsers() {
-                return this.listOfUsers.filter(el => el.name.toLowerCase().includes(this.searchQuery.toLowerCase()));
+                //return this.listOfUsers.filter(el => el.name.toLowerCase().includes(this.searchQuery.toLowerCase()));
+                const filteredByName = this.listOfUsers.filter(user => user.name.toLowerCase().includes(this.searchQuery.toLowerCase()));
+                if (!this.filteredType) {
+                    return filteredByName;
+                }
+                return filteredByName.filter(user => user.role === this.filteredType);
             }
         }
 
